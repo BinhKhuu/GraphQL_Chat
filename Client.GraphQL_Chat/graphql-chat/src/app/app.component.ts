fd9daccf7ef6430579088777b0dcc8a8db9ee8eb
Binh Khuu <binhkhuu90@gmail.com>
2025-10-18 14:33:32 +0800

    add basic material ui styling

--- Client.GraphQL_Chat/graphql-chat/src/app/app.component.ts
+++ Client.GraphQL_Chat/graphql-chat/src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'graphql-chat';
+  messages: string[] = [];
+  newMessage = '';
+
+  sendMessage() {
+    if (this.newMessage.trim()) {
+      this.messages.push(this.newMessage.trim());
+      this.newMessage = '';
+    }
+  }
 }
